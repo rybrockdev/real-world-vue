ac1400cb48e4bc572b69e77f302704e9d8ea8c80
Ry Brockers <rybrockers@github.com>
2020-03-15 17:18:00 +0000

    Added error displaying on screen. Finished project

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -3,18 +3,17 @@
     <div class="event-header">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h1 class="title">{{ event.title }}</h1>
-      <h5>Organized by {{ event.organizer }}</h5>
+      <h5>Organized by {{ event.organizer ? event.organizer.name : '' }}</h5>
       <h5>Category: {{ event.category }}</h5>
     </div>
-
-    <BaseIcon name="map"><h2>Location</h2></BaseIcon>
-
+    <BaseIcon name="map">
+      <h2>Location</h2>
+    </BaseIcon>
     <address>{{ event.location }}</address>
-
     <h2>Event details</h2>
     <p>{{ event.description }}</p>
-
-    <h2>Attendees
+    <h2>
+      Attendees
       <span class="badge -fill-gradient">{{ event.attendees ? event.attendees.length : 0 }}</span>
     </h2>
     <ul class="list-group">
@@ -25,24 +24,17 @@
   </div>
 </template>
 <script>
-import EventService from '@/services/EventService.js'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   props: ['id'],
-  data() {
-    return {
-      event: {}
-    }
-  },
   created() {
-    EventService.getEvent(this.id)
-      .then(response => {
-        this.event = response.data
-      })
-      .catch(error => {
-        console.log('There was an error:', error.response)
-      })
-  }
+    this.fetchEvent(this.id)
+  },
+  computed: mapState({
+    event: state => state.event.event
+  }),
+  methods: mapActions('event', ['fetchEvent'])
 }
 </script>
 <style scoped>
